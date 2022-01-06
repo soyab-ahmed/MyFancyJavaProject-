ac6eecc10f44f921c412475a48a07c14a543a229
soyab-ahmed <sahmed182033@bscse.uiu.ac.bd>
2022-01-06 22:06:51 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static int id = 182033;
+    public static String name = "Soyab Ahmed";
+
+    public Main(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Id of x is " + id );
+        System.out.println("My name is " + name);
+    }
+}
