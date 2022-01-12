4da4bdb6e5da22c31f2a5e6d600099dcceb89450
soyab-ahmed <sahmed182033@bscse.uiu.ac.bd>
2022-01-12 19:51:30 +0600

    commit of y

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,13 @@
 import java.sql.SQLOutput;
 
 public class Main {
-    public  int id ;
-    public  String name ;
+    private  int id ;
+    private   String name ;
 
-    public Main(int id, String name){
+    private Main(int id, String name){
         this.id = id;
         this.name = name;
     }
 
-    public static void main(String[] args) {
-        Main x = new Main(182033,"Soyab Ahmed");
-        System.out.println("Id of x is " + x.id );
-        System.out.println("My name is " + x.name);
-        System.out.println("For 6 january commit.");
-    }
+
 }
