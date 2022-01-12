8bb4fecd8012731ff0905c5e8498abe43c529385
soyab-ahmed <sahmed182033@bscse.uiu.ac.bd>
2022-01-12 20:37:29 +0600

    singleton pattern get and set

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,19 @@ public class Main {
         this.name = name;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
